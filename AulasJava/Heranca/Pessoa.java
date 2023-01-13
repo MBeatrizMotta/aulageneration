bbf8568957aba8a3a9358315082907fb20378c76
MBeatrizMotta <mariabeatrizmottapino@gmail.com>
2023-01-12 23:11:47 -0300

    Adicionando os Exercicios

--- AulasJava/Heranca/Pessoa.java
+++ AulasJava/Heranca/Pessoa.java
@@ -76,4 +76,56 @@ public class Pessoa {
 		}
 	}
 		
+		package POO;
+
+public class Empregado extends Pessoa{
+	
+	private int codigoSetor;
+	private float salarioBase;
+	private float imposto;
+	
+	public Empregado(String nome,String endereco,String cpf,int telefone,int idade,
+			int codigoSetor,float salarioBase,float imposto) {
+		super(nome,endereco,cpf,telefone,idade);
+		this.codigoSetor = codigoSetor;
+		this.salarioBase = salarioBase;
+		this.imposto = imposto;
+	}
+
+	public int getCodigoSetor() {
+		return codigoSetor;
+	}
+
+	public void setCodigoSetor(int codigoSetor) {
+		this.codigoSetor = codigoSetor;
+	}
+
+	public float getSalarioBase() {
+		return salarioBase;
+	}
+
+	public void setSalarioBase(float salarioBase) {
+		this.salarioBase = salarioBase;
+	}
+
+	public float getImposto() {
+		return imposto;
+	}
+
+	public void setImposto(float imposto) {
+		this.imposto = imposto;
+	}
+	
+	public void imprimirInfo() {
+		System.out.println("\nNome do empregado: "+getNome()+"\nCPF: "+getCpf()+"\nIdade: "+getIdade()+
+				"\nTelefone: "+getTelefone()+"\nEndereço:"+getEndereco()+"\nCódigo do setor: "+codigoSetor+
+				"\nSalário base: "+salarioBase+"\nValor em porcentagem (sem o %) de imposto a ser retido do salário: "+imposto);
 		
+	}
+	
+	public void calcularSalario() {
+		double salario_total = salarioBase - (salarioBase *(imposto/100));
+		System.out.println("\nO salário líquido a ser recebido pelo empregado "+getNome()+" é igual a: "+salario_total);
+	}
+
+}
